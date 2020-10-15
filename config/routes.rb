b6c673d91ca438c64239dc7478b0b189e6f7391b
yutaide317 <yutaide317@icloud.com>
2020-10-15 20:16:25 +0900

    time_field_change

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,5 @@
 Rails.application.routes.draw do
   devise_for :users
-  
-  get 'maple_waffles/index'
-  get 'lemon_waffles/index'
-  get 'matcha_waffles/index'
-  get 'choco_waffles/index'
-  get 'plain_waffles/index'
   resources :orders, only: [:index, :new, :create]
   resources :plain_waffles, only: :index
   resources :choco_waffles, only: :index
