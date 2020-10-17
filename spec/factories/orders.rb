f9b667b64cf132a2d847cf44e39d00c72c586f00
yutaide317 <yutaide317@icloud.com>
2020-10-17 13:55:17 +0900

    test1

---
+++ spec/factories/orders.rb
@@ -0,0 +1,10 @@
+FactoryBot.define do
+
+  factory :order do
+    plain              {"1"}
+    signup_at                 {"2020-12-19"}
+    time              {"12:36:00"}
+    total       {"150"}
+  end
+
+end
